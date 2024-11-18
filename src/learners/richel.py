7144ead718b12fc8d139f249b8266f84243128fb
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-11-18 21:16:32 +0100

    is_zero raises an exception on nonsense input

--- src/learners/richel.py
+++ src/learners/richel.py
@@ -6,7 +6,17 @@ def is_zero(x):
     Will return False if the input is an integer with a non-zero.
     Will raise an exception if the input type is not an integer
     """
+    if not isinstance(x, int):
+        raise TypeError("'x' must be of type int")
     return x == 0
 
 assert is_zero.__doc__
 assert is_zero(0) == True
+
+# Must raise an exception if the input type is not an integer
+has_thrown = False
+try:
+    is_zero("nonsense")
+except:
+    has_thrown = True
+assert has_thrown
