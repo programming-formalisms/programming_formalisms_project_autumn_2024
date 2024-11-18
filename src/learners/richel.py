c4bb79f206193d088f4aaa37244d80ab4781b68d
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-11-18 21:20:43 +0100

    Undo exercise `is_zero` :-)

--- src/learners/richel.py
+++ src/learners/richel.py
@@ -1,22 +1,5 @@
-def is_zero(x):
-    """
-    Determines if input is zero.
-    
-    Will return True if the input is an integer with value zero.
-    Will return False if the input is an integer with a non-zero.
-    Will raise an exception if the input type is not an integer
-    """
-    if not isinstance(x, int):
-        raise TypeError("'x' must be of type int")
-    return x == 0
+"""Richel's code."""
 
-assert is_zero.__doc__
-assert is_zero(0) == True
-
-# Must raise an exception if the input type is not an integer
-has_thrown = False
-try:
-    is_zero("nonsense")
-except:
-    has_thrown = True
-assert has_thrown
+def get_name():
+    """Get Richel's name, spelled correctly."""
+    return "Richèl"
