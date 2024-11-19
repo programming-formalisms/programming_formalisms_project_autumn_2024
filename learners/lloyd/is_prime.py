683ead57b43a76d69cb10b6dadfce3df795eb77a
Gustav.Jonsson97 <oddevold58@gmail.com>
2024-11-19 13:57:14 +0100

    Found error

--- learners/lloyd/is_prime.py
+++ learners/lloyd/is_prime.py
@@ -1,7 +1,10 @@
-def is_prime(x)
+def is_prime(x):
     """
     Determines the input number whether it is prime
     True for prime
     False for not prime
     Raise error for non-integer
     """
+
+assert is_prime.__doc__
+assert is_prime(1) == True
