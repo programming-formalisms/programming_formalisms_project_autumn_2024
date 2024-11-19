5e55b098b86bfdb70f71cba727cc81b4ebc8ac43
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-11-19 13:13:06 +0100

    is_one has documentation

--- src/learners/is_one.py
+++ src/learners/is_one.py
@@ -1,1 +1,12 @@
+def is_one(x):
+    """
+    Determine if the input 'x' is one.
+
+    Returns True if x is 1
+    Returns False if x is not 1
+    Raise an exception when the input is not an integer
+    """
+
 assert is_one.__doc__
+
+
