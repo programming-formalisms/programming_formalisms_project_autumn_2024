a6a3ae3482eb01d05d57ae3b47c7b2112f6ce35a
Anneli Nilsson <anneli.nilsson@umu.se>
2024-11-19 11:58:45 +0100

    Finished is_zero function.

--- src/learners/anneli.py
+++ src/learners/anneli.py
@@ -4,8 +4,22 @@ def is_zero(x):
     Will return False if input is not zero.
     Will raise an exception if input type is not integer."""
 
+    if not isinstance(x, int):
+        raise TypeError("'x' must be of type int")
     if x == 0:
         return True
+    elif x != 0:
+        return False
+
 
 assert is_zero.__doc__
 assert is_zero(0) == True
+print(is_zero(0))
+
+# must raise an exception if the input type is not an integer
+has_thrown = False
+try:
+    is_zero("nonsense")
+except:
+    has_thrown = True
+assert has_thrown
