d59149933fce207be39d396ce556cd83780a24b3
Emilia <emimoralest@gmail.com>
2024-11-19 13:36:39 +0100

    changes

---
+++ src/learners/pam_em.py
@@ -0,0 +1,6 @@
+def is_even(x):
+    """ Function to indicate True if the input number is an even number (can be divided by 2)
+    and False if the number is uneven, (can't be divided by 2)
+    """
+
+assert is_even.__doc__
