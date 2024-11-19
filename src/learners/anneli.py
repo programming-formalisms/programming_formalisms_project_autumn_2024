5e7a489a1d0169a75ce9225c1682774bf26d9eae
Anneli Nilsson <anneli.nilsson@umu.se>
2024-11-19 11:44:06 +0100

    is_zero works when 0 is input

--- src/learners/anneli.py
+++ src/learners/anneli.py
@@ -1,7 +1,11 @@
-def is_zero():
+def is_zero(x):
     """Determines if input is zero.
     Will return True if input is zero.
     Will return False if input is not zero.
     Will raise an exception if input type is not integer."""
 
+    if x == 0:
+        return True
+
 assert is_zero.__doc__
+assert is_zero(0) == True
