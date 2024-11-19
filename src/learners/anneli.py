ca96190f53d23b022d336252b8a2a49756f93061
Anneli Nilsson <anneli.nilsson@umu.se>
2024-11-19 11:33:35 +0100

    Creating anneli.py and documentation for is_zero.

---
+++ src/learners/anneli.py
@@ -0,0 +1,7 @@
+def is_zero():
+    """Determines if input is zero.
+    Will return True if input is zero.
+    Will return False if input is not zero.
+    Will raise an exception if input type is not integer."""
+
+assert is_zero.__doc__
