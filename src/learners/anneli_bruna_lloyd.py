2ca2850b198cd57e87bb8f877b80b12d96841f4f
llvillanueva <lloyd.villanueva@chalmers.se>
2024-11-20 14:00:53 +0100

    updated anneli_bruna_lloyd.py

--- src/learners/anneli_bruna_lloyd.py
+++ src/learners/anneli_bruna_lloyd.py
@@ -1,5 +1,10 @@
 def is_number(x):
     """Function that returns True if input is a number and False if it is not a number.
     Raises exception if input is not a number."""
+    if not isinstance(x, float):
+        return False
+    return True
 
 assert is_number.__doc__
+assert is_number(1.5) == True
+assert is_number("string") == False
