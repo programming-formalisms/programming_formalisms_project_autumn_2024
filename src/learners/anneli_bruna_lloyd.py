0727dd5cfa47607a1bc7a228064967a89f22d28f
annelinilsson <anneli.nilsson@umu.se>
2024-11-20 13:53:25 +0100

    test

---
+++ src/learners/anneli_bruna_lloyd.py
@@ -0,0 +1,5 @@
+def is_number(x):
+    """Function that returns True if input is a number and False if it is not a number.
+    Raises exception if input is not a number."""
+
+assert is_number.__doc__
