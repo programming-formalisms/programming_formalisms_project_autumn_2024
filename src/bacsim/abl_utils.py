51ae1c43a312567d1759557cc33b40fffb047985
llvillanueva <lloyd.villanueva@chalmers.se>
2024-11-20 15:21:58 +0100

    added abl_utils.py

---
+++ src/bacsim/abl_utils.py
@@ -0,0 +1,8 @@
+def is_number(x):
+    """Function that returns True if input is a number and False if it is not a number.
+    Raises exception if input is not a number."""
+    if  isinstance(x, float):
+        return True
+    elif isinstance(x, int):
+        return True
+    return False
