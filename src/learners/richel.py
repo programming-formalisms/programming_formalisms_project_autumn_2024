4471537a68facd603df90d5fbc22f2ecc6eec9f0
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-11-21 13:01:56 +0100

    EOD

--- src/learners/richel.py
+++ src/learners/richel.py
@@ -10,3 +10,22 @@ def isprime(num):
         if num%n==0:
             return False
     return True
+
+
+
+
+
+class Range:
+    def __init__(self, any_lowest, any_highest):
+        assert any_lowest <= any_highest
+        self._lowest = any_lowest
+        self._highest = any_highest
+    def get_lowest(self):
+        return self._lowest
+    def get_highest(self):
+        return self._highest
+
+x = Range(3, 10)
+assert x.get_lowest() == 3
+assert x.get_highest() == 10
+Range(100, 10) # Must raise an exception
