6b1faaaaf1be811ddc067fa2e656b5e460dba901
piiamt <piiamaria.tomberg@gmail.com>
2024-11-21 10:31:00 +0200

    something

--- tests/test_piia_utils.py
+++ tests/test_piia_utils.py
@@ -1,23 +1,18 @@
 """Tests all code in src.bacsim.piia_utils."""
 import unittest
 
-from src.bacsim.piia_utils import is_zero
+from src.bacsim.piia_utils import isprime
 
 class TestPiiaUtils(unittest.TestCase):
 
     """Class to test the code in src.bacsim.piia_utils."""
 
-    def test_is_zero_has_documentation(self):
-        """The function 'is_zero' has documentation."""
-        self.assertTrue(is_zero.__doc__)
-        self.assertIsNotNone(is_zero.__doc__)
+    def test_isprime_responds_correctly_to_ints(self):
+        """The function 'isprime' responds correctly to integers."""
+        self.assertTrue(isprime(7))
+        self.assertFalse(isprime(8))
 
-    def test_is_zero_responds_correctly_to_ints(self):
-        """The function 'is_zero' responds correctly to integers."""
-        self.assertTrue(is_zero(0))
-        self.assertFalse(is_zero(1))
-
-    def test_is_zero_raises_an_exception_upon_non_ints(self):
-        """The function 'is_zero' raises an exception upon non-ints."""
-        self.assertRaises(TypeError, is_zero, {1, 2})
-        self.assertRaises(TypeError, is_zero, "I am a string")
+    def test_isprime_raises_an_exception_upon_non_ints(self):
+        """The function 'isprime' raises an exception upon non-ints."""
+        self.assertRaises(TypeError, isprime, {1, 2})
+        self.assertRaises(TypeError, isprime, "I am a string")
