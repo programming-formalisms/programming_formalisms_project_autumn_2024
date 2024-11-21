c1b44bb3852841662c1e35d8403c9c77a3bc13c9
Gustav.Jonsson97 <oddevold58@gmail.com>
2024-11-21 15:39:03 +0100

    fixked code style

--- tests/test_gustav.py
+++ tests/test_gustav.py
@@ -20,11 +20,12 @@ def is_prime(x):
 
 
 class TestIsPrime(unittest.TestCase):
-    def test_prime_numbers(self)
+    def test_prime_numbers(self):
         self.assertTrue(is_prime(2))
         self.assertFalse(is_prime(4))
         self.assertTrue(is_prime(13))
         self.assertTrue(is_prime(101))
 
 if __name__ == "__main__":
+    
     unittest.main()
